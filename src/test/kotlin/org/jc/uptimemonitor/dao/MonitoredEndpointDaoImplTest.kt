0b5950a6d562a4f6185254e4d34aa021d2ba8d86
JC Mac <justinching365@gmail.com>
2026-02-17 00:37:37 -0700

    updating changes in test classes

--- src/test/kotlin/org/jc/uptimemonitor/dao/MonitoredEndpointDaoImplTest.kt
+++ src/test/kotlin/org/jc/uptimemonitor/dao/MonitoredEndpointDaoImplTest.kt
@@ -71,6 +71,7 @@ class MonitoredEndpointDaoImplTest {
         active = true,
         createdAt = LocalDateTime.of(2026, 1, 1, 0, 0),
         email = "user@example.com",
+        endpointId = 12345,
         expectedResponse = null,
         expectedStatusCode = 200,
         frequency = Frequency.DAILY,
